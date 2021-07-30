52cb3bcbcf405b82bbf0ec12d806dff8c05ed2e6
Justin Turner <jmturner638@gmail.com>
2021-07-30 07:02:43 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    finish = int(input("Where to?"))
+    start = int(input("Where from?"))
+
+    for i in range(start, finish+1):
+        print(i)
 
 if __name__ == '__main__':
     main()
